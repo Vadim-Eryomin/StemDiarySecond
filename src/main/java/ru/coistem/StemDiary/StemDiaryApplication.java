57b32b2fda2ac00e1424e0864ac25669922503b4
Vadim <vadim.eremin.2005@mail.ru>
2020-08-26 12:59:22 +0600

    heroku first deploy;

--- src/main/java/ru/coistem/StemDiary/StemDiaryApplication.java
+++ src/main/java/ru/coistem/StemDiary/StemDiaryApplication.java
@@ -8,9 +8,15 @@ import java.sql.SQLException;
 @SpringBootApplication
 public class StemDiaryApplication {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws Exception {
 		new Thread(() -> SpringApplication.run(StemDiaryApplication.class, args)).start();
 		Database.connect();
+		if(!Database.query("SELECT * FROM login").next()) {
+			Database.query("INSERT INTO login VALUES(1, 'admin', '111')");
+			Database.query("INSERT INTO profile VALUES(1, 'Admin', 'Admin', 'img/noroot.png', 111)");
+			Database.query("INSERT INTO login VALUES(1, 't', 't')");
+		}
+
 	}
 
 }
